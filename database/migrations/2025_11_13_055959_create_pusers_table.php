0649cd34e15297f6ad0ccd4b3500ed7bcf17a7cb
sitaaraman <sitaaraman2003@gmail.com>
2025-11-13 12:30:52 +0530

    Initial Commit

---
+++ database/migrations/2025_11_13_055959_create_pusers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pusers', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->string('username')->unique();
+            $table->string('email');
+            $table->string('gender');
+            $table->date('dateofbirth');
+            $table->string('phone_number', 15);
+            $table->string('profile')->nullable();
+            $table->string('password');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pusers');
+    }
+};
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'StoryTale') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+        <!-- Scripts -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    </head>
+    <body class="font-sans antialiased">
+        {{-- <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
+
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div> --}}
+
+        <header>
+        <nav class="navbar navbar-expand-lg" style="background-color: #10193b;">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="{{ asset('images/logo/phototalelogo.png') }}" style="width:3em; height:3em;"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('postuser.index') }}" style="color: #e7f2ef;">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('post.create') }}" style="color: #e7f2ef;">Photo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" style="color: #e7f2ef;">Tale</a>
+                        </li>
+                        <li class="nav-item">
+                            @if (!session()->has('user'))
+                                <a class="nav-link" href="{{ route('postuser.login') }}" style="color: #e7f2ef;">Welcome! Guess.</a>
+                            @else
+                                <a class="nav-link" href="{{ route('postuser.profile', [session('user')->slug]) }}" style="color: #e7f2ef;">Welcome! {{ session('user')->fullname }}.</a>
+                            @endif
+                            
+                        </li>
+                        <li class="nav-item">
+                            @if (!session()->has('user'))
+                                <a class="nav-link" href="{{ route('postuser.login') }}" style="color: #e7f2ef;">Login/Registration</a>
+                            @else
+                                <a class="nav-link" href="{{ route('postuser.logout') }}" style="color: #e7f2ef;">Logout</a>
+                            @endif
+                            
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main class="bg-warning-subtle mh-100">
+        @yield('content')
+    </main>
+
+    <footer class="text-center text-lg-start" style="background-color: #a1c2bd;">
+        <div class="container d-flex justify-content-center py-5">
+            <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+                <i class="fab fa-facebook-f"></i>
+            </button>
+            <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+                <i class="fab fa-youtube"></i>
+            </button>
+            <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+                <i class="fab fa-instagram"></i>
+            </button>
+            <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+                <i class="fab fa-twitter"></i>
+            </button>
+        </div>
+ 
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2); color: #19183b;">
+            © 2025 Copyright:
+            <a href="#" style="color: #19183b; text-decoration:none;">photoTale Using Laravel 12</a>
+        </div>
+    </footer>
+    </body>
+</html>
